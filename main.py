ea66596d468b1ec485d05cfb24370a5e71ced16f
Nathan Pannell <tekkbot@gmail.com>
2020-08-15 01:41:55 -0700

    simplified mentions and added decorator chaining

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class RemBotClient(discord.Client):
             return
         for command in self.commands:
             if message.content.startswith(command[0]):
-                await command[1](self, message)
+                await command[1](self, command[0], message)
 
     def register(self, name):
         def register_command(func):
@@ -23,6 +23,7 @@ class RemBotClient(discord.Client):
                     self.commands.append((n,func))
             else:
                 self.commands.append((name, func))
+            return func
         return register_command
 
     async def on_ready(self):
@@ -31,23 +32,17 @@ class RemBotClient(discord.Client):
 client = RemBotClient()
 
 @client.register('r/')
-async def reddit(client, message):
-    await message.channel.send(f'https://www.reddit.com/{message.content}')
+async def reddit(client, command, message):
+    await message.channel.send(f'https://reddit.com/{message.content}')
 
 @client.register(['re:nick', 're:name'])
-async def renick(client, message):
+async def renick(client, command, message):
     if len(message.mentions) == 1:
         member = message.mentions[0]
         old_name = member.display_name
         print(message.content)
-        if '!' not in member.mention:
-            mention_with_bang = member.mention.replace('@', '@!')
-            mention = member.mention
-        else:
-            mention_with_bang = member.mention
-            mention = member.mention.replace('!', '')
         mention = member.mention
-        new_name = re.match(rf're:(?:nick|name)\s+(?:{mention}|{mention_with_bang})\s*(.*)', message.content).group(1)
+        new_name = re.match(rf'{command}\s+<@!?\d+>\s*(.*)', message.content).group(1)
         try:
             await member.edit(nick=new_name)
         except discord.errors.HTTPException as e:
