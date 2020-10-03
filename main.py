beef741398adce99941b6052b683205bf72657f6
Nathan Pannell <tekkbot@gmail.com>
2020-10-03 00:52:44 -0700

    ethan added thiccen and wide

--- main.py
+++ main.py
@@ -5,6 +5,35 @@ import parsedatetime
 from datetime import datetime
 import asyncio
 import random
+import json
+
+from string import ascii_lowercase
+from string import ascii_uppercase
+
+# shamelessly stolen from the OCF ircbot ocf.io/gh/ircbot
+WIDETEXT_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
+
+# the space character has unique mapping to become a unicode ideographic space
+WIDE_SPACE_VALUE = 0x3000
+WIDE_SPACE_CHAR = chr(WIDE_SPACE_VALUE)
+SPACE_VALUE = ord(' ')
+WIDETEXT_MAP[SPACE_VALUE] = WIDE_SPACE_VALUE
+
+# As seen in Samurai Jack
+THICC = '卂乃匚刀乇下厶卄工丁长乚从几口尸㔿尺丂丅凵リ山乂丫乙'
+
+
+# Little hack, because translation tables are just dicts
+LOWER_THICC = {k: v for k, v in str.maketrans(ascii_lowercase, THICC).items() if v is not None}
+UPPER_THICC = {k: v for k, v in str.maketrans(ascii_uppercase, THICC).items() if v is not None}
+THICC_MAP = {
+    **LOWER_THICC,
+    **UPPER_THICC,
+    SPACE_VALUE: WIDE_SPACE_VALUE,
+}
+
+def change_text(text, translation, width=0):
+    return ''.join((c.translate(translation) + WIDE_SPACE_CHAR * width for c in text))
 
 
 with open('/usr/share/dict/words') as f:
@@ -104,7 +133,9 @@ async def remind(client, command, message):
     
     if match := re.match(rf'{command}:?\s+(.*)\s+(?:\"(.*)\").*', message.content):
         time, msg = match.groups()
-        time_struct, parse_status = client.cal.parse(time)
+        time_struct, status = client.cal.parse(time)
+        if status == 0:
+            await message.channel.send(f'Failed to set the re:mind-er. Could not understand the time you specified.')
         delta = datetime(*time_struct[:6]) - datetime.now()
         secs = delta.total_seconds()
         key = random.choice(WORDS).lower()  # random word from the system wordlist
@@ -119,5 +150,40 @@ async def remind(client, command, message):
         else:
             await message.channel.send(f'There is no re:mind-er {key}!')
 
+@client.register(['re:thiccen', 'thiccen'])
+async def wide(client, command, message):
+    if match := re.match(rf'{command}:?\s+(.*)', message.content):
+        await message.channel.send(f'{change_text(match.group(1), THICC_MAP, 0)}')
+
+@client.register(['re:wide', '!w'])
+async def thiccen(client, command, message):
+    if match := re.match(rf'{command}(\d?):?\s+(.*)', message.content):
+        if match.group(1) != '':
+            width = int(match.group(1)) - 1
+        else:
+            width = 0
+        await message.channel.send(f'{change_text(match.group(2), WIDETEXT_MAP, width)}')
+
+# @client.register('re:tz', 'tz')
+# async def tz(client, command, message):
+#     # {userid: tz, user2: tz}
+#     if match := re.match(rf'{command}:?\s*(.*)'):
+#         with open('timezones.json', 'r+') as tz_json:
+#             user_tz = json.read(tz_json.read())
+#             user_tz[message.author.id] = match.group(1)
+#             tz_json.write(json.dump(user_tz))
+
+
+# @client.register('')
+# async def detect_time(client, command, message):
+#     time_struct, status = client.cal.parse(message.content)
+#     if (status == 0):
+#       return
+#     dt = datetime(*time_struct[:6])
+#     with open('timezones.json', 'r') as tz_json:
+#         user_tz = json.read(tz_json.read())
+#         tz = user_tz[message.author.id]
+#     await message.channel.send(f"That's:\n")
+    
 
 client.run(TOKEN)
